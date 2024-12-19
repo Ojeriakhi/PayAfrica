6beb5f27da376d644abf54ffced0f144999266e6
Ojeriakhi <joyojeey@gmail.com>
2024-12-19 13:15:26 +0100

    initial commit

---
+++ pages/home.vue
@@ -0,0 +1,5 @@
+<template>
+ <div class="bg-pink-500">
+    hsdcjh,adbchilj
+ </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,296 @@
+<template>
+   <section class="py-10">
+     <nav class="flex justify-between px-10 pb-2 ">
+        <div>
+           <img src="/public/images/PayAfrica.png" alt="logo">
+        </div>
+        <div>
+            <ul class="text-[#40196D] text-lg flex justify-between  gap-10">
+                <li>Features</li>
+                <li>Business</li>
+                <li class="border shadow-md rounded-md py-1 -mt-1 px-2 bg-white  border-[#40196D40] -ml-6">Beta</li>
+                <li>Company</li>
+                <li>Help</li>
+            </ul>
+
+        </div>
+        <div>
+            <button class="text-[#40196D] p-4">Sign In </button>
+            <button class="text-white bg-[#070600] px-10 py-4 rounded-lg ">Join PayAfrica</button>
+
+        
+
+        </div>
+      </nav>
+      <section class="bg-[#40196D] py-32 px-24 flex justify-between h-[530px] ">
+        <div>
+            <h1 class=" text-white text-5xl ">Banking Across Africa <br> Made Simple.</h1>
+        <p class="text-lg text-white mt-3">Our cutting-edge technology enables you to carry out transactions <br> securely and effortlessly, free from the stress of currency exchange <br> or high transaction fees.</p>
+        <button class="text-white bg-[#070600] mt-4 p-4 px-12 rounded-lg">Join PayAfrica</button>
+
+        </div>
+        <div class="-mt-16 relative overflow-hidden h-[467px] ">
+            <img src="/public/images/phone.png" alt="" class=" ">
+        </div>
+      </section>
+
+      <section class="mt-10 px-10 py-6 bg-white">
+        <h2 class="text-[#40196D] text-4xl text-center font-medium ">Unlock endless possibilities with our virtual <br> cards.</h2>
+        <p class="  text-center text-[#070600BF] text-lg mt-4">Say goodbye to the limitations of traditional banking and experience the freedom of a truly modern <br> financial solution.</p>
+      </section>
+      <section class="mt-10 flex gap-16 px-10 py-6 justify-center ">
+
+        <div>
+            <img src="/public/images/card1.png" alt="">
+
+            <div class="mt-10">
+                <h3 class="text-[#40196D] text-xl font-medium">Simplify your banking with <br> PayAfrica Pro.</h3>
+                <p class="mt-4 text-[#070600BF] text-sm">Don't waste your time and money on <br> unnecessary fees and complicated <br> banking processes. </p>
+                <button class="mt-4 text-[#40196D] text-sm">Learn More</button>
+            </div>
+
+        </div>
+
+        <div>
+            <img src="/public/images/card2.png" alt="">
+            <div class="mt-10">
+                <h3 class="text-[#40196D] text-xl font-medium">Shop the world with our <br> virtual dollar card.</h3>
+                <p class="mt-4 text-[#070600BF] text-sm">With our virtual dollar card, shopping <br> globally is now at your fingertips. Our card <br> is accepted everywhere Mastercard cards <br> are accepted, giving you the freedom to <br> shop online without any limitations. </p>
+                <button class="mt-4 text-[#40196D] text-sm">Learn More</button>
+            </div>
+
+        </div>
+
+        <div>
+            <img src="/public/images/card3.png" alt="">
+            <div class="mt-10">
+                <h3 class="text-[#40196D] text-xl font-medium">Pay with ease, settle with <br> speed.</h3>
+                <p class="mt-4 text-[#070600BF] text-sm">Our innovative payment platform makes it <br> easy for you to make payments and settle <br> bills quickly and securely.  </p>
+                <button class="mt-4 text-[#40196D] text-sm">Learn More</button>
+            </div>
+
+        </div>
+      </section>
+
+      <section class="px-40">
+        <div class="mt-10 flex justify-between">
+            <div class="mt-32">
+               <h3 class="text-[#40196D] txet-2xl ">Smart Saving</h3>
+               <p class="pt-2">We know that saving money for the future is a wise decision,<br> but it can be challenging to maintain the discipline needed to <br> consistently put money aside. That's why we've harnessed <br> the power of technology to automate the saving process for<br> you. With PayAfrica, you can easily set an amount to save,<br> and our app will take care of the rest. No need to worry about <br>forgetting or struggling to stick to a savings plan.</p>
+            </div>
+
+            <div>
+                <img src="/public/images/piggybank.png" alt="">
+
+            </div>
+
+        </div>
+      </section>
+
+      <section class="px-40">
+        <div class="mt-10 flex justify-between">
+            <div>
+                <img src="/public/images/EWallet.png" alt="">
+
+            </div>
+
+            <div class="mt-32">
+               <h3 class="text-[#40196D] txet-2xl ">More ways to pay</h3>
+               <p class="pt-2">PayAfrica has got you covered! With our various payment <br> options, including a widely accepted debit card issued in <br> partnership with Visa, quick web payments directly from <br> your PayAfrica account, and location-based gift cards for <br> shopping and subscriptions, you can choose the option that <br> suits you best.</p>
+            </div>
+
+            
+        </div>
+      </section>
+
+      <section class="px-40">
+        <div class="mt-10 flex justify-between">
+           
+
+            <div class="mt-32">
+               <h3 class="text-[#40196D] txet-2xl ">Simplify your payments</h3>
+               <p class="pt-2">With PayAfrica, you can easily find and connect with your <br> friends to make unlimited instant transfers for free. And <br> when it comes to sending money to  local banks, you can do <br> so without breaking a sweat. Even fulfilling promises to <br> family and loved ones across borders has never been easier. <br> At PayAfrica, we're dedicated to making your financial life <br> simple and stress-free.</p>
+            </div>
+            <div>
+                <img src="/public/images/simplify.png" alt="">
+
+            </div>
+
+            
+        </div>
+      </section>
+
+      <section class="mx-32 ">
+        <div class="mt-10 bg-[#EFF1FF] px-32 pt-16 flex justify-between shadow-lg rounded-lg"> 
+            <div class="mt-8">
+                <h3 class="text-[#40196D] text-4xl font-medium">Benefits of Using Our <br> Services</h3>
+               <p class="">Easily transfer and receive funds in your local <br> currency across Africa, without geographical <br> limitations or constraints.</p>
+               <div class="mt-8">
+                  <button class="text-white bg-[#070600] px-8 py-3 rounded-lg">Join PayAfrica</button>
+                </div>
+            </div>
+            
+            <div>
+                <img src="/public/images/Onlineworld.png" alt="">
+            </div>
+         
+        </div>
+      </section>
+
+      <section class="mx-32">
+        <div class="mt-20 flex gap-4 ">
+           <button>
+            <img src="/public/images/iosbtn.png" alt="">
+           </button>
+           <button>
+            <img src="/public/images/playstorebtn.png" alt="">
+           </button>
+           <span class="text-[#979797] ml-32">Products may vary by country or market.</span>
+        </div>
+      </section>
+      
+
+      <section class="mt-20 mx-32">
+        <div class="flex gap-6 "> 
+            <div class="mt-24">
+                <img src="/public/images/PayAfrica.png" alt="">
+            </div>
+            <div class="grid grid-cols-4 gap-24">
+                <div class="flex flex-col gap-2">
+                    <div>
+                        <button class="text-[#40196D] text-sm">Features</button> 
+                    </div>
+                    <div>
+                        <button class="text-sm">PayAfrica Card</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Investment</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Spend</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Save</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Budget</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Borrow</button>
+
+                    </div>
+
+                </div>
+                <div class="mt-4 flex flex-col gap-2">
+                    <div>
+                        <button class="text-[#40196D] text-sm">Company</button> 
+                    </div>
+                    <div>
+                        <button class="text-sm">Blog</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Press</button>  
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Join Our Team</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">About Us</button>
+
+                    </div>
+                    
+                </div>
+                <div class="mt-4 flex flex-col gap-2">
+                    <div>
+                        <button class="text-[#40196D] text-sm">Help</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Get Help</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">FAQs</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Security</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Contact Us</button> 
+
+                    </div>
+                </div>
+                <div class="flex flex-col gap-2">
+                    <div>
+                        <button class="text-[#40196D] text-sm">Transparency</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Terms & Conditions</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Privacy Policy</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Information Security Policy</button> 
+                    </div>
+                    <div>
+                        <button class="text-sm">Cookie Policy</button> 
+
+                    </div>
+                    <div>
+                        <button class="text-sm">Compliants</button>
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+      </section>
+
+      <section class="mt-10 text-[#40196D] text-sm px-32 flex flex-col">
+        <span>Contact</span>
+        <span class="mt-4">help@PayAfrica.com</span>
+      </section>
+      
+      <section class="mt-10 px-32 flex gap-4">
+        <button class="p-4 rounded-full bg-[#EFF1FF]">
+            <img src="/public/images/x.png" alt="">
+        </button>
+        <button class="p-4 rounded-full bg-[#EFF1FF]">
+            <img src="/public/images/IG.png" alt="">
+        </button>
+        <button class="p-4 rounded-full bg-[#EFF1FF]">
+            <img src="/public/images/FB.png" alt="">
+        </button>
+        <button class="p-4 rounded-full bg-[#EFF1FF]">
+            <img src="/public/images/YT.png" alt="">
+        </button>
+        <button class="p-4 rounded-full bg-[#EFF1FF]">
+            <img src="/public/images/M.png" alt="">
+        </button>
+        <button class="p-4 rounded-full bg-[#EFF1FF]">
+            <img src="/public/images/IN.png" alt="">
+        </button>
+
+      </section>
+
+      <section class="px-32 mt-10">
+        <span class="text-sm">© 2023 PayAfrica Technologies Ltd (Company No.11472232). All rights reserved.</span>
+        <p class="text-sm mt-4">If you would like to find out more about which PayAfrica entity you receive services from, please reach out to us via the in-app chat in the PayAfrica app. Nigerian banking <br>services offered by PayAfrica Bank (RC796975) with registered address at 151 - Herbert Macaulay Way, Yaba, Lagos, Nigeria. </p>
+
+      </section>
+   </section>
+</template>
